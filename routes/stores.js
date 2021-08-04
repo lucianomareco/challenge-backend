665749cc8f10d86be4c31d03f521b5c853996e1f
luciano <lucianogastonmareco@gmail.com>
2021-08-04 19:57:45 -0300

    route createStore added

--- routes/stores.js
+++ routes/stores.js
@@ -2,9 +2,13 @@ const logger = require('../utils/logger');
 const express = require('express');
 const router = express.Router();
 const StoreSchema = require('../models/store');
-const { getStores } = require('../services/stores');
+const {
+  getStores,
+  createStore
+} = require('../services/stores');
 
 router.route('/stores').get(getStores);
 
+router.route('/stores').post(createStore);
 
 module.exports = router;
