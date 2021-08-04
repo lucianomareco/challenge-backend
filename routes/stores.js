585775868b60e0fd538d77f9f4b4aa0966d8bb5d
luciano <lucianogastonmareco@gmail.com>
2021-08-04 18:34:20 -0300

    route generate removed

--- routes/stores.js
+++ routes/stores.js
@@ -1,33 +1,10 @@
 const logger = require('../utils/logger');
 const express = require('express');
-const { route } = require('../app');
 const router = express.Router();
 const StoreSchema = require('../models/store');
-const { getData } = require('../services/stores');
+const { getStores } = require('../services/stores');
 
-router.route('/stores')
-  .get(async (req, res) => {
-    const limit = req.query.limit;
-    const page = req.query.page;
-    dataFound = await getData(limit, page);
-    res.send(dataFound);
-  }
-  );
+router.route('/stores').get(getStores);
 
-router.route('/stores/generate').
-  get((req, res) => {
-    for (let i = 0; i < 1000; i++) {
-      const store = new StoreSchema;
-      store.name = `Name${i}`;
-      store.cuit = `Cuit${i}`;
-      store.concept = [];
-      store.currentBalance = i;
-      store.active = true;
-      store.save(err => {
-        if (err) { return next(err); }
-      });
-    }
-    res.send('stores generated')
-  })
 
 module.exports = router;
