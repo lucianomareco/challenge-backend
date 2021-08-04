163e43a63f1786a2c975a8adc9ba766c0dc12d8d
luciano <lucianogastonmareco@gmail.com>
2021-08-04 19:58:50 -0300

    createStore created

--- services/stores.js
+++ services/stores.js
@@ -1,12 +1,30 @@
 const StoreSchema = require('../models/store');
 
+
+
 const getStores = async (req, res) => {
     const params = req.query.q;
     const { page, limit } = JSON.parse(params);
+    const total = await StoreSchema.count({});
+    const pages = Math.ceil(total / limit);
     const storesFound = await StoreSchema.find().limit(limit);
-    res.send(storesFound)
+    const response = {
+        data: storesFound,
+        pages: pages,
+        limit: limit,
+        total: total
+    }
+    res.send(response)
 }
 
+const createStore = async (req, res) => {
+    console.log(req.body);
+    const store = new StoreSchema(req.body);
+    const storeSaved = await store.save();
+    res.json(storeSaved);
+};
+
 module.exports = {
-    getStores
+    getStores,
+    createStore
 }
