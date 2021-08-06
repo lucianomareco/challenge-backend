ffe7b129c92b925ea2fa551fd5abf7eadb75d73f
luciano <lucianogastonmareco@gmail.com>
2021-08-06 11:10:39 -0300

    console.log removed

--- utils/middleware/basicAuth.js
+++ utils/middleware/basicAuth.js
@@ -4,17 +4,14 @@ const UserSchema = require('../../models/user')
 const autentication = async (req, res, next) => {
     try {
         const user = await auth(req);
-        console.log(user.name)
 
         const userFound = await UserSchema.findOne({ username: user.name })
-        console.log(userFound)
         if (!userFound) {
             res.status(401).json({
                 error: 'invalid user or password'
             })
         }
         if (userFound.verifyPassword(user.pass)) {
-            console.log('auth: success');
             next();
         } else {
             res.status(401).json({
