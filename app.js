e80e31451322c863fcd2748555ce89d2bdb986a6
luciano <lucianogastonmareco@gmail.com>
2021-08-06 15:40:31 -0300

    import mw

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 const logger = require('./utils/logger');
 mongoose.Promise = Promise;
-const notFound = require('./utils/middleware/notFound');
-const handleErrors = require('./utils/middleware/handleErrors');
+const notFound = require('./utils/middlewares/notFound');
+const handleErrors = require('./utils/middlewares/handleErrors');
 
 const express = require('express')
 const app = express()
@@ -18,7 +18,7 @@ app.use(notFound)
 app.use(handleErrors)
 
 // Start the server
-app.listen(config.get('port'));
+server = app.listen(config.get('port'));
 logger.info('API initialized on port ' + config.get('port'));
 
-module.exports = app
+module.exports = { app, server }
