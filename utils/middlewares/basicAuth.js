5797565513d9b4421b6cb197e73a26cd9f5d2b68
luciano <lucianogastonmareco@gmail.com>
2021-08-06 21:36:57 -0300

    refactor AuthError

--- utils/middlewares/basicAuth.js
+++ utils/middlewares/basicAuth.js
@@ -1,26 +1,27 @@
 const auth = require('basic-auth');
 const UserSchema = require('../../models/user')
+const AuthError = require('../errors/authError');
 
-const autentication = async (req, res, next) => {
+const authentication = async (req, res, next) => {
     try {
         const user = await auth(req);
 
+        if (!user) {
+            throw new AuthError('invalid user or password');
+        }
+
         const userFound = await UserSchema.findOne({ username: user.name })
         if (!userFound) {
-            res.status(401).json({
-                error: 'invalid user or password'
-            })
+            throw new AuthError('invalid user or password');
         }
         if (userFound.verifyPassword(user.pass)) {
             next();
         } else {
-            res.status(401).json({
-                error: 'invalid user or password'
-            })
+            throw new AuthError('invalid user or password')
         }
     } catch (err) {
         next(err)
     }
 }
 
-module.exports = autentication;
+module.exports = authentication;
--- utils/middlewares/handleErrors.js
+++ utils/middlewares/handleErrors.js
@@ -8,12 +8,16 @@ const ERROR_HANDLERS = {
   QueryValidationError: (res, { message }) =>
     res.status(409).send({ error: message }),
 
+  AuthError: (res, { message }) =>
+    res.status(401).send({ error: message }),
+
   MongoError: (res) =>
     res.status(409).send({ error: 'invalid query' }),
 
   defaultError: (res, error) => {
     console.error(error.name)
-    console.error(error.message)
+    console.log(error.message)
+    console.log(error.stack)
     res.status(500).send("Internal error")
   }
 }
